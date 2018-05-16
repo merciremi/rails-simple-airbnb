968ff80ace4ec5de15e442a593ed8cdb9a7fdee4
Remi Mercier <remi.merci@gmail.com>
2018-05-16 16:38:43 +0200

    Create routes, controller and views for flats

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+p 'Creating 10 flats'
+
+10.times do
+  Flat.create!(
+  name: Faker::HitchhikersGuideToTheGalaxy.starship,
+  address: Faker::Address.street_address,
+  description: Faker::HitchhikersGuideToTheGalaxy.quote,
+  price_per_night: rand(40..250),
+  number_of_guests: rand(1..10)
+  )
+end
+
+p 'All done 👌'
+
